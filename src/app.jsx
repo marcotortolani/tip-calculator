5d22804964d90977cbfc0d657f95b6e6c8d089f5
Marco Tortolani <mjtorto@gmail.com>
2023-07-29 11:54:43 -0300

    fix footer responsive

--- src/app.jsx
+++ src/app.jsx
@@ -162,15 +162,22 @@ export function App() {
 
       <footer>
         <div className="attribution">
-          Challenge by{" "}
-          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-            Frontend Mentor
-          </a>
-          . Coded by{" "}
-          <a href="https://mtorto.com" target="_blank">
-            Marco Tortolani
-          </a>
-          .
+          <div>
+            Challenge by{" "}
+            <a
+              href="https://www.frontendmentor.io?ref=challenge"
+              target="_blank"
+            >
+              Frontend Mentor
+            </a>
+          </div>
+          <span> - </span> 
+          <div>
+            Coded by{" "}
+            <a href="https://mtorto.com" target="_blank">
+              Marco Tortolani
+            </a>
+          </div>
         </div>
       </footer>
     </div>
