8cf3d2a73dd85fd14761cb4e41abb81bf1774aec
Marco Tortolani <mjtorto@gmail.com>
2023-08-03 14:30:16 -0300

    feat menu color selector

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import "./app.scss";
-import { useContext } from "preact/hooks";
+import { useContext, useState } from "preact/hooks";
 import { StateProvider } from "./StateProvider.jsx";
 import Attribution from "./components/Attribution";
 import Calculator from "./components/Calculator";
@@ -13,18 +13,25 @@ export function App() {
     setUserConfig,
     colorOptions,
   } = useContext(ConfigContext);
+  const [showMenuColor, setShowMenuColor] = useState(false);
 
   const handleChangeTheme = (e) => {
+    const color = userConfig.color;
     e.target.checked
-      ? setUserConfig({theme: "light", color: userConfig.color})
-      : setUserConfig({theme: "dark", color: userConfig.color});
-      
+      ? setUserConfig({ theme: "light", color })
+      : setUserConfig({ theme: "dark", color });
+  };
+
+  const handleMenuColor = () => {
+    showMenuColor ? setShowMenuColor(false) : setShowMenuColor(true);
   };
 
   const handleSelectColor = (e) => {
     const selectedOption = e.target.value;
-    //setUserColor(selectedOption);
-    setUserConfig({color: selectedOption, theme: userConfig.theme})
+    const theme = userConfig.theme;
+
+    setUserConfig({ theme, color: selectedOption });
+    setShowMenuColor(false);
   };
 
   return (
@@ -48,24 +55,30 @@ export function App() {
         <div className="wrapper-set-color">
           {/* <h2>Set Color</h2> */}
           <div className="select-menu horizontal chang">
-            <select data-menu="horizontal" onChange={handleSelectColor}>
-              {colorOptions.map((colorOpt, index) => {
-                return (
-                  <option
-                    key={index}
-                    selected={colorOpt === userConfig.color ? true : false}
-                  >
-                    {colorOpt}
-                  </option>
-                );
-              })}
-            </select>
-            {/* <button onClick={handleSelectColor} style="--h: 19px; --w: 64px;">
-              <em></em>
-              <div>
-                <span className="">{userConfig.color}</span>
+            <div className={`dropdown ${showMenuColor ? "active" : "hide"}`}>
+              <input
+                onClick={handleMenuColor}
+                className="textBox"
+                type="text"
+                readOnly
+                placeholder=""
+                value={userConfig.color}
+              />
+              <div className="options">
+                {colorOptions.map((colorOpt, index) => {
+                  return (
+                    <button
+                      key={index}
+                      className="btn-option"
+                      onClick={(e) => handleSelectColor(e)}
+                      value={colorOpt}
+                    >
+                      {colorOpt}
+                    </button>
+                  );
+                })}
               </div>
-            </button> */}
+            </div>
           </div>
         </div>
       </header>
