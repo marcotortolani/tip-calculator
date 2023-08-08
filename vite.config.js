72dd7b2e904e55595ca12facda06dce264045399
Marco Tortolani <mjtorto@gmail.com>
2023-08-07 21:18:07 -0300

    feat readme.md

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
-})
+  plugins: [
+    preact(),
+    VitePWA({
+      injectRegister: "inline",
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      includeAssets: ["./public/logo.png"],
+      manifest: {
+        name: "Tip Calculator",
+        short_name: "TipCalc",
+        lang: "en",
+        description:
+          "App for calculating tips, designed by Frontend Mentor, developed by Marco Tortolani.",
+        theme_color: "#000",
+        icons: [
+          {
+            src: "./public/logo.png",
+            sizes: "92x92",
+            type: "image",
+          },
+        ],
+      },
+    }),
+  ],
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
+});
